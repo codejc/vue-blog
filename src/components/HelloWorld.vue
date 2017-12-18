06177300a947811d47e881b4f79e21a6a40cdc37
Jincong Hong <764454663@qq.com>
2017-12-18 12:09:52 +0800

    eslint 规范

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -22,13 +22,13 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: "HelloWorld",
+    data() {
+        return {
+            msg: "Welcome to Your Vue.js App"
+        };
     }
-  }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
