f41dda55028a4ffc584220b3216e6954db6807ca
Jincong Hong <764454663@qq.com>
2017-12-18 14:32:40 +0800

    搭建server

---
+++ src/pages/login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="jc-login">
+        <div class="jc-login-card">
+            <el-input v-model="account" placeholder="请输入账号"></el-input>
+            <el-input v-model="password" placeholder="请输入密码"></el-input>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data() {
+        return {
+            account: "",
+            password: ""
+        };
+    }
+};
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import Router from "vue-router";
 import HelloWorld from "@/components/HelloWorld";
-
+import Login from "@/pages/login.vue";
 Vue.use(Router);
 
 export default new Router({
     routes: [
         {
             path: "/",
-            name: "HelloWorld",
-            component: HelloWorld
+            name: "Login",
+            component: Login
         }]
 });
