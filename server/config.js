9be67c60e7797fe5f1a5444e76c231fd04284f45
Jincong <764454663@qq.com>
2018-03-24 17:21:53 +0800

    fix: edit production host

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ import path from "path";
 export default {
     // 服务器配置
     db: {
-        host: "localhost",
+        host: "193.112.27.192",
         user: "root",
         password: "123456",
         database: "vue-blog"
@@ -12,5 +12,5 @@ export default {
     jwtSecret: "www.codejc.com",
     apiVersion: "api/v1",
     assetsPath: path.join(__dirname, "assets"),
-    host: "193.112.27.192:8888"
+    host: "http://193.112.27.192:8888"
 };
